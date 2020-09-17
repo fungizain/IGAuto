643b4b2bc1526a673f49443c357d94b35882a89c
fungizain <49989439+fungizain@users.noreply.github.com>
2020-09-17 13:32:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
+import shutil
 from selenium import webdriver
 
-path_to_chrome = '/usr/local/bin/chromedriver'
-
-DRIVER = webdriver.Chrome(path_to_chrome)
+HOST = 'https://www.instagram.com/'
+LOGIN_URL = HOST + 'accounts/login/?source=auth_switcher/'
 
 TIMEOUT = 30
 
-HOST = 'https://www.instagram.com/'
-
-LOGIN_URL = HOST + 'accounts/login/?source=auth_switcher/'
+path_to_chrome = shutil.which('chromedriver')
+DRIVER = webdriver.Chrome(path_to_chrome)
